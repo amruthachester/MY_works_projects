de0b2bbffb891944676b6d44ce840bfe836699f3
Your Name <amruthavps@gmail.com>
2025-10-20 23:00:59 +0100

    checked the value of x and y

--- week1/week2.py
+++ week1/week2.py
@@ -4,3 +4,12 @@ if a>b:
     print("a is greater than b")
 else:
     print("a is less than b")
+x=input("enter the value of x")
+y=input("enter the value of y")
+if x>y:
+    print("x is greater than y")
+elif x==y:
+    print("x is equal to y")
+else:
+    print("x is less than y")
+
