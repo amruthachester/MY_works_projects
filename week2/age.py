04931fdab1d3a7462f3e5e90712dbdd9ea8870dc
Your Name <amruthavps@gmail.com>
2025-10-24 10:20:40 +0100

    Age is checked

---
+++ week2/age.py
@@ -0,0 +1,5 @@
+age=int(input("Enter the age of the person"))
+if age >= 18:
+    print("the person is adult and is able to vote")
+else:
+    print("the person is minor and not able to give the vote")
